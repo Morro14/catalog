d69c125a6b802b5b99e77547074209876a4aa671
Ivan <ivfmn1@gmail.com>
2025-07-18 18:47:55 +0400

    add tests

---
+++ server/tests/pass_change_test.py
@@ -0,0 +1,9 @@
+import httpx
+
+
+URL = "http://127.0.0.1:8000/"
+
+
+r = httpx.post(url=URL + "auth/password-recovery", data={"email": "email1@test.com"})
+print(r.cookies)
+print("Pass recovery response:", r)
