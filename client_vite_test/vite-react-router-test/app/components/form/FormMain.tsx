5df5cb1e2afa46d3729deb649df3a111a716e7d7
Ivan <ivfmn1@gmail.com>
2025-09-19 13:29:17 +0400

    remove test app

--- client_vite_test/vite-react-router-test/app/components/form/FormMain.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import Input from "./Input";
-import { Outlet } from "react-router";
-
-export default function Form() {
-	const [content, setContent] = useState();
-
-	return <></>;
-}
--- client_vite_test/vite-react-router-test/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-	content: ["./app/**/*.{js,ts,jsx,tsx}"], // 👈 ensure app/ is included
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-} satisfies Config;
